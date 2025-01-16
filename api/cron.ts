2df9ae2ea8d872317622985bbbb2b484b9f15178
Alexander Noriega <alexander.noriega@qubika.com>
2025-01-16 12:53:05 -0300

    refactor: Rename.

--- api/cron.ts
+++ api/cron.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { handleChatMessage } from "../lib/manatal-slack-integration";
+import { run } from "../lib/manatal-slack-integration";
 
 /*
 const logUnknownError = (msg: string, e: unknown) => {
