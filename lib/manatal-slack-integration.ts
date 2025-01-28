d49486e4232369c76f9e634d32dc7ac713437e7a
Alexander Noriega <alexander.noriega@qubika.com>
2025-01-28 13:12:19 -0300

    feat: Format.

--- lib/manatal-slack-integration.ts
+++ lib/manatal-slack-integration.ts
@@ -4,7 +4,8 @@ import { postToSlack } from "./slack";
 const LOG_CTX = "[lib/manatal-slack-integration]" as const;
 
 const formatMatch = (job: Job.Match): string => {
-  return `*${job.position}*\n${job.candidate} - ${job.stage_name}`;
+  const bu = job.business_unit;
+  return `${bu !== null ? `(${bu}) ` : ""}*${job.position}*\n${job.candidate} - ${job.stage_name}`;
 };
 
 export const run = async () => {
