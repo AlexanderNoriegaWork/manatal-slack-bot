bf0c6223e33548a272814a82a842edcddd5b4a63
Alexander Noriega <alexander.noriega@qubika.com>
2025-01-28 12:59:08 -0300

    feat: Format.

--- lib/manatal-slack-integration.ts
+++ lib/manatal-slack-integration.ts
@@ -9,6 +9,18 @@ const formatMatch = (job: Job.Match): string => {
 
 export const run = async () => {
   const r = await getRecruitmentInfo();
-  const formatted = r.map(formatMatch).join("\n\n");
-  await postToSlack(formatted);
+  const dict: Record<Job.BusinessUnit | "all", Job.Match[]> = {};
+  r.forEach((job) => {
+    const bu = job.business_unit || "all";
+    dict[bu] = dict[bu] !== undefined ? dict[bu].concat([job]) : [job];
+  });
+  const message: string = Object.keys(dict).reduce(
+    (acc: string, bu: Job.BusinessUnit | "all") => {
+      const formatted = dict[bu].map(formatMatch).join("\n\n");
+      return acc + `*${bu.toUpperCase()}*` + formatted;
+    },
+    "",
+  );
+  // const formatted = r.map(formatMatch).join("\n\n");
+  await postToSlack(message);
 };
